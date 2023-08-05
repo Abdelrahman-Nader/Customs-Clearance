7156ee6494973c42172c3a10d14a35c7530f01cc
“abdo” <abualiya.2202@gmail.com>
2023-08-05 08:33:07 +0300

    creat main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenutabComponent } from './sheard/menutab/menutab.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { DataEntryComponent } from './component/data-entry/data-entry.component';
+import { CommunicationComponent } from './component/layout for air line&comm/communication/communication.component';
+import { AirlineDataComponent } from './component/layout for air line&comm/airline-data/airline-data.component';
+import { FooterComponent } from './sheard/footer/footer.component';
+import { LayoutComponent } from './component/layout/layout.component';
+import { ViewLayoutComponent } from './component/layout for air line&comm/view-layout/view-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenutabComponent,
+    DataEntryComponent,
+    CommunicationComponent,
+    AirlineDataComponent,
+    FooterComponent,
+    LayoutComponent,
+    ViewLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
+    RouterOutlet,
     NgbModule,
 
 
---
+++ src/app/component/layout for air line&comm/airline-data/airline-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airline-data',
+  templateUrl: './airline-data.component.html',
+  styleUrls: ['./airline-data.component.css']
+})
+export class AirlineDataComponent {
+
+}
